dee34bed1d7771526408d04911fcec4a03512ce4
justmindingmyownshit <justmindingmyownshit.com>
2025-04-06 00:21:36 +0530

    chore5

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -1,7 +1,10 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../constants";
 
-const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+const baseQuery = fetchBaseQuery({
+  baseUrl: BASE_URL,
+  credentials: "include", // ✅ Important: allows sending cookies (for JWT)
+});
 
 export const apiSlice = createApi({
   baseQuery,
